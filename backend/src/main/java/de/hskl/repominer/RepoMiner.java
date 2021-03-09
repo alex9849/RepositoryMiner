1f2ba00180a4386aa0476a21e452b9c38bfb04ad
alex9849 <alexander@liggesmeyer.net>
2021-03-09 17:38:54 +0100

    Added basic spring boot

---
+++ backend/src/main/java/de/hskl/repominer/RepoMiner.java
@@ -0,0 +1,11 @@
+package de.hskl.repominer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepoMiner {
+    public static void main(String[] args) {
+        SpringApplication.run(RepoMiner.class, args);
+    }
+}
