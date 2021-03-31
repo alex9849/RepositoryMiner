f8a3aa4a57fb3bfff954ef36c6b0d36ca1b7cd75
alex9849 <alexander@liggesmeyer.net>
2021-03-31 12:31:24 +0200

    updated logparser

--- backend/src/main/java/de/hskl/repominer/service/logparser/LogParser.java
+++ backend/src/main/java/de/hskl/repominer/service/logparser/LogParser.java
@@ -56,21 +56,10 @@ public class LogParser {
                 int deletedLines = isMerge? 0:pFc.deletions;
                 FileChange currentFileChange = new FileChange(0, 0, path, addedLines, deletedLines);
 
-                File file;
-                if(pFc.isRename()) {
-                    file = fileTracker.changeName(pc.hash, pFc.oldPath, pFc.newPath);
-                } else {
-                    file = fileTracker.getFile(pc.hash, pFc.newPath);
-                }
+                File file = fileTracker.getFile(pc.hash, pFc.oldPath);
 
                 currentFileChange.setFile(file);
                 fileChanges.add(currentFileChange);
-            }
-            for(String filePath : pc.changedFiles.deletedFiles) {
-                fileTracker.onDeleteFile(pc.hash, filePath);
-            }
-            for(String filePath : pc.changedFiles.createdFiles) {
-                fileTracker.onCreateFile(pc.hash, filePath);
             }
 
             currentCommit.setFileChanges(fileChanges);
@@ -152,90 +141,4 @@ public class LogParser {
         return parsedCommits;
     }
 
-/*
-    public static void parseLogStream(BufferedReader logInputStream) throws IOException, ParseException {
-        Project project = new Project();
-        project.setAuthors(new ArrayList<>());
-        project.setCommits(new ArrayList<>());
-
-        Map<String, Author> nameToAuthors = new HashMap<>();
-        Map<String, File> pathToFileMap = new HashMap();
-        SimpleDateFormat dateParser = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
-
-        for (UnparsedCommit currentUnparsedCommit : unparsedCommits) {
-            Scanner headerScanner = new Scanner(currentUnparsedCommit.header);
-            String stringHash = headerScanner.findInLine("[0-9a-z]+");
-            String stringAuthor = headerScanner.findInLine(" \\[.+?\\] ");
-            stringAuthor = stringAuthor.substring(2, stringAuthor.length() - 2);
-            String stringDate = headerScanner.findInLine("\\d\\d\\d\\d-\\d\\d-\\d\\d \\d\\d:\\d\\d:\\d\\d");
-            String commitMessage = headerScanner.findInLine(".+").trim();
-            String finalStringAuthor = stringAuthor;
-            Author author = nameToAuthors.computeIfAbsent(stringAuthor, x -> new Author(0, 0, finalStringAuthor));
-            Commit commit = new Commit(0, 0, stringHash, isMainBranch, 0, new Date(dateParser.parse(stringDate).getTime()), commitMessage);
-            commit.setAuthor(author);
-            List<FileChange> fileChanges = new ArrayList<>();
-            for(String fileChangeText : currentUnparsedCommit.modifiedFiles) {
-                Scanner fileModifyScanner = new Scanner(fileChangeText);
-                int addedLines = 0;
-                if(fileModifyScanner.hasNextInt()) {
-                    addedLines = fileModifyScanner.nextInt();
-                } else {
-                    fileModifyScanner.next();
-                }
-                int deletedLines = 0;
-                if(fileModifyScanner.hasNextInt()) {
-                    deletedLines = fileModifyScanner.nextInt();
-                } else {
-                    fileModifyScanner.next();
-                }
-                String fileName = fileModifyScanner.findInLine(".+").trim();
-                File changedFile;
-                String path;
-                if(!fileName.matches(".+ =\\> .+")) {
-                    changedFile = pathToFileMap.computeIfAbsent(fileName, x -> new File(0, 0));
-                    if(currentUnparsedCommit.deletedFiles.contains(fileName)) {
-                        //If a file gets deleted, we store null
-                        path = null;
-                    } else {
-                        path = fileName;
-                    }
-                } else {
-                    String[] fileNames = getOldAndNewPath(fileName);
-                    //old filename
-                    changedFile = pathToFileMap.remove(fileNames[0]);
-                    pathToFileMap.put(fileNames[1], changedFile);
-                    path = fileNames[1];
-                }
-                FileChange fileChange = new FileChange(0, 0, path, addedLines, deletedLines);
-                fileChange.setFile(changedFile);
-                fileChanges.add(fileChange);
-            }
-            commit.setFileChanges(fileChanges);
-            project.getCommits().add(commit);
-        }
-        project.getAuthors().addAll(nameToAuthors.values());
-        return project;
-    }
-
-    private static String[] getOldAndNewPath(String renameString) {
-        String pathPrefix = "";
-        String pathSuffix = "";
-        boolean isVeryLongRename = renameString.matches("(.+)?\\{.+=>.+}(.+)?");
-        if(isVeryLongRename) {
-            String[] bracketWrappers = renameString.split("\\{.+=>.+}", 2);
-            pathPrefix = bracketWrappers[0];
-            pathSuffix = bracketWrappers[1];
-            renameString = renameString.substring(pathPrefix.length() + 1, renameString.length() - pathSuffix.length() - 1);
-        }
-        String[] renamedPart = renameString.split(" => ", 2);
-        String oldPath = (pathPrefix + renamedPart[0] + pathSuffix).replace("//", "/");
-        String newPath = (pathPrefix + renamedPart[1] + pathSuffix).replace("//", "/");
-        return new String[]{
-                oldPath,
-                newPath
-        };
-
-    }
-
- */
 }
