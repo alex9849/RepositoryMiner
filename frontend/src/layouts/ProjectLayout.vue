0770db76e16c0013f7fafdd37b6f6f953e1ce754
alex9849 <alexander@liggesmeyer.net>
2021-04-06 11:22:30 +0200

    Added project layout and browseproject page

---
+++ frontend/src/layouts/ProjectLayout.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page padding>
+    <q-page-container>
+      <router-view/>
+    </q-page-container>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: "ProjectLayout"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/pages/BrowseProject.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page
+    padding
+  >
+
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: "BrowseProject"
+}
+</script>
+
+<style scoped>
+
+</style>
