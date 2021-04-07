f149fe4a9915c8f1b1d6b8c5b9d7bbad7f772f2a
alex9849 <alexander@liggesmeyer.net>
2021-04-07 21:44:19 +0200

    Added Highcharts default config

--- frontend/src/boot/highcharts-vue.js
+++ frontend/src/boot/highcharts-vue.js
@@ -1,5 +1,42 @@
-import HighchartsVue from 'highcharts-vue'
+import HighchartsVue from 'highcharts-vue';
+import Highcharts from 'highcharts';
+
+const defaultOptions = {
+  chart: {
+    plotBackgroundColor: null,
+    plotBorderWidth: null,
+    plotShadow: false,
+    type: 'pie'
+  },
+  title: {
+    text: null
+  },
+  tooltip: {
+    pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+  },
+  accessibility: {
+    point: {
+      valueSuffix: '%'
+    }
+  },
+  plotOptions: {
+    pie: {
+      allowPointSelect: true,
+      cursor: 'pointer',
+      dataLabels: {
+        enabled: true,
+        format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+      }
+    },
+    packedBubble: {
+      layoutAlgorithm: {
+        enableSimulation: false
+      }
+    }
+  }
+};
 
 export default ({ Vue }) => {
   Vue.use(HighchartsVue);
+  Highcharts.setOptions(defaultOptions);
 };
