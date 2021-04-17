6dce45234898d497a6f1bf1bfed323564fade3c5
alex9849 <alexander@liggesmeyer.net>
2021-04-17 11:27:39 +0200

    Added Settings layout

--- frontend/src/pages/ProjectSettings.vue
+++ frontend/src/pages/ProjectSettings.vue
@@ -1,10 +1,56 @@
 <template>
+  <q-page>
+    <div
+      class="row q-col-gutter-lg"
+    >
+      <div
+        class="col-12 col-md-3"
+      >
+        <q-list
+          bordered
+          separator
+          class="rounded-borders"
+        >
+          <q-item
+            clickable
+            v-ripple
+            v-for="section of menuSections"
+          >
+            <q-item-section>
+              <q-item-label>
+                {{ section.name }}
+              </q-item-label>
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </div>
 
+      <div
+        class="col-12 col-md"
+      >
+        <div
+          class="bg-amber-1 rounded-borders"
+        >
+          Content
+        </div>
+      </div>
+    </div>
+  </q-page>
 </template>
 
 <script>
 export default {
-  name: "ProjectSettings"
+  name: "ProjectSettings",
+  data() {
+    return {
+      menuSections: [
+        {
+          name: 'authors',
+          route: {name: ''}
+        }
+      ]
+    }
+  }
 }
 </script>
 
