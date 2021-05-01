6301e848ba46fc8a595cb1318727188a7dbab4bb
alex9849 <alexander@liggesmeyer.net>
2021-05-01 14:21:43 +0200

    changed colorspectrum of heatmap

--- frontend/src/service/chartServices/HeatMapChartService.js
+++ frontend/src/service/chartServices/HeatMapChartService.js
@@ -47,9 +47,7 @@ class HeatMapChartService {
       },
       colorAxis: {
         stops: [
-          [0, '#3060cf'],
-          [0.5, '#fffbbc'],
-          [0.9, '#c4463a'],
+          [0, '#f5f0f0'],
           [1, '#c4463a']
         ],
         startOnTick: false,
